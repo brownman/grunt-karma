44531568379b94ea112a64936cd18c314df87efb
brownman <brownman556@gmail.com>
2014-11-16 17:12:44 +0200

    Create aggregation_test.js

---
+++ test/aggregation_test.js
@@ -0,0 +1,11 @@
+describe('aggregation', function(){
+
+  describe('one', function(){
+    it('show load the source file', function(){
+      console.log('one');
+      expect(aggregated).to.be.a('string');
+    });
+  });
+ 
+
+});
