74c9f10cbb01ae82a6bc619b5c2bdc515763a974
brownman <brownman556@gmail.com>
2014-11-16 17:24:01 +0200

    Create aggregation_start.js

---
+++ src/aggregation_start.js
@@ -0,0 +1,23 @@
+
+var aggregated = {
+  header: {
+    js: {
+      data: null,
+      weights: []
+    },
+    css: {
+      data: null,
+      weights: []
+    }
+  },
+  footer: {
+    js: {
+      data: null,
+      weights: []
+    },
+    css: {
+      data: null,
+      weights: []
+    }
+  }
+};
