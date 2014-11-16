6440036ad914379c7649e2a398756bbc3af2520e
brownman <brownman556@gmail.com>
2014-11-16 17:28:54 +0200

    Update aggregation_test.js

--- test/aggregation_test.js
+++ test/aggregation_test.js
@@ -3,7 +3,7 @@ describe('aggregation', function(){
   describe('one', function(){
     it('show load the source file', function(){
       console.log('one');
-      expect(aggregated).to.be.a('string');
+      expect(aggregated).to.be.a('Object');
     });
   });
  
